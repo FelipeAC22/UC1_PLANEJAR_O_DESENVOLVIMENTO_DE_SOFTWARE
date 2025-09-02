6d7477a45d25851b8b44331af5bccb96a2a6318b
Felipe Antunes Cardoso <felipeac2204@gmail.com>
2025-09-01 21:52:16 -0300

    Add files via upload

---
+++ algoritmo.js
@@ -0,0 +1,18 @@
+// faezndo café //
+
+let primeiroPasso = " 1. Coloque água na chaleira \n"
+let segundoPasso = "2. Aqueça até ferver \n"
+let terceiroPasso = "3. Coloque café no filtro \n"
+let quartoPasso = "4. Despeje água quente no café \n"
+let quintoPasso = "5. Adicione leite \n"
+let sextoPasso = "Sirva!"
+
+// utilizando o console.log para exibir as informações //
+
+console.log(primeiroPasso, segundoPasso, terceiroPasso, quartoPasso, quintoPasso)
+
+// array do café //
+
+let prepararCafé = [ "1. Coloque água na chaleira", "2. Aqueça até ferver", "3. Coloque café no filtro", "4. Despeje água quente no café", "5. Adicione leite", "6. Sirva!" ]
+
+console.table(prepararCafé)
